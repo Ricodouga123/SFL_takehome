69416063d0eddd81c67415ecf719e412871f136d
Rico <Ricodouga123@gmail.com>
2022-07-15 08:44:40 -0500

    db_init ready

--- db_init.py
+++ db_init.py
@@ -1,7 +1,21 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import ForeignKey, create_engine, MetaData, Table, Column, Integer, String
 
 engine = create_engine('sqlite:///sfl_db.db')
 
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
+meta = MetaData()
+
+gender = Table('gender', meta,
+    Column('id', Integer, primary_key = True),
+    Column('gender_type', String(25), nullable = False, unique = True)
+)
+
+user = Table( 'user', meta, 
+    Column('id', Integer, primary_key = True),
+    Column('first_name', String(16), nullable = False),
+    Column('last_name', String(16), nullable = False),
+    Column('email_address', String(60), nullable = False),
+    Column('gender_id', ForeignKey("gender.id"), nullable = True),
+    Column('ip_address', String(25), nullable = False)
+)
+
+meta.create_all(engine)
